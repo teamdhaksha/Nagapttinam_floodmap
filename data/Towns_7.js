f6dd1f4c7604a805f260c567d25991eb03b06d5f
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-02-04 12:01:25 +0530

    Add files via upload

---
+++ data/Towns_7.js
@@ -0,0 +1,15 @@
+var json_Towns_7 = {
+"type": "FeatureCollection",
+"name": "Towns_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Vedaranyam" }, "geometry": { "type": "Point", "coordinates": [ 79.857657415728141, 10.373003203149439, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Nagapattinam" }, "geometry": { "type": "Point", "coordinates": [ 79.842004885456774, 10.76752667160345, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Tharangambadi" }, "geometry": { "type": "Point", "coordinates": [ 79.849459331296273, 11.031500938102379, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Thirumalaivasal" }, "geometry": { "type": "Point", "coordinates": [ 79.837289179207147, 11.242833753017271, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Sirkazhi" }, "geometry": { "type": "Point", "coordinates": [ 79.732975535266874, 11.23984916710199, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kuttalam" }, "geometry": { "type": "Point", "coordinates": [ 79.560918241125592, 11.07566460934709, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Mayiladuthurai" }, "geometry": { "type": "Point", "coordinates": [ 79.647997537194186, 11.09942162386567, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Velankanni" }, "geometry": { "type": "Point", "coordinates": [ 79.839028538480392, 10.682317786909749, -999999.0 ] } }
+]
+}
